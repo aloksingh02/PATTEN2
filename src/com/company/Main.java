2bfe65c281a6d4f95fb147ee60f4e07c1eb45a52
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-02-18 10:25:54 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=65;
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print((char)(a+j)+" ");
+            }
+            System.out.println();
+        }
+        for(int i=5;i>0;i-- )
+        {
+            for(int  j=0;j<i;j++)
+            {
+                System.out.print((char)(a+j)+" ");
+            }
+            System.out.println();
+        }
+	// write your code here
+    }
+}
